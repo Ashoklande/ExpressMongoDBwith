20e2c2f0cc5c0cb649299e8ae07ab6850be9106f
ashoka <ashoklande73@gmail.com>
2024-11-15 12:27:30 +0530

    Add specific files

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/',(req,res)=>{
 });
 
 app.get('/create',async(req,res)=>{
-    
+    //insering new user into mongodb
     let usercreate= await usermodel.create({
         name:'Ashok Lande',
         email:'@gmail.com',
@@ -25,11 +25,21 @@ app.get('/create',async(req,res)=>{
     res.send(usercreate);
 });
 
-
+//finding user who's Age=20;
 app.get('/find',async(req,res)=>{
     let userone=await usermodel.findOne({Age:20});
 
     res.send(userone);
+
+})
+
+//updating the existing record ;
+
+app.get('/update',async(req,res)=>{
+    let updateduser= await usermodel.updateOne({email:'@gmail.com'},{$set:{name:'Onkar Torane'}});
+
+    res.send(updateduser);
+   
 })
 app.listen(3000,function(){
     console.log('Server start');
